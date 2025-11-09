ebcbf8887782108002218673ecbeda27fd5edd98
ngochoaphan2004 <phanngochoa2004@gmail.com>
2025-11-09 17:56:19 +0700

    feat: add swagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,31 +1,45 @@
-const swagger_config = (app, PORT) => {
-    const swaggerUi = require('swagger-ui-express');
-    const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require('swagger-jsdoc');
+const path = require('path');
 
-    const swaggerOptions = {
-        definition: {
-            openapi: '3.0.0',
-            info: {
-                title: 'Smart Farm Backend API',
-                version: '1.0.0',
-                description: 'API documentation for Smart Farm backend using Express and Swagger',
-                contact: {
-                    name: 'Developer',
-                    email: 'you@example.com'
-                },
-            },
-            servers: [
-                {
-                    url: `http://localhost:${PORT}`,
-                    description: 'Local development server'
-                }
-            ],
+const projectId = 'btl-he-thong-nhung';
+const region = 'us-central1';
+
+const serverUrl = `http://localhost:5001/${projectId}/${region}/app`;
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Smart Farm Backend API',
+      version: '1.0.0',
+      description: 'API documentation for Smart Farm backend using Express and Swagger',
+    },
+    servers: [
+      {
+        url: serverUrl,
+        description: 'Firebase Emulator (Local)'
+
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
         },
-        apis: ['./routes/*.js', './index.js'],
-    };
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: [
+    path.join(__dirname, '\\swagger\\*.js'),
+  ],
+};
 
-    const swaggerSpec = swaggerJsdoc(swaggerOptions);
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-}
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
-module.exports = swagger_config
+module.exports = {
+  swaggerOptions,
+  swaggerSpec
+};
