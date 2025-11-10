ecb182282dffa09585279d0f2a27cdd70a91045d
ngochoaphan2004 <phanngochoa2004@gmail.com>
2025-11-10 15:09:12 +0700

    rm: rm key in authen

--- config/authenticateToken.js
+++ config/authenticateToken.js
@@ -7,7 +7,7 @@ const authenticateToken = (req, res, next) => {
 
     if (token == null) return res.sendStatus(401); 
 
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    jwt.verify(token, "your_secret_key", (err, user) => {
         if (err) return res.sendStatus(403); 
 
         req.user = user;
